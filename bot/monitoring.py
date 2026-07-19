4b660557e23c90d196badb1086b4bea64f8927fe
EstebanHuriot <esteban.martin@bsb-education.com>
2026-07-19 11:57:01 +0200

    Update MonitoringState

--- bot/monitoring.py
+++ bot/monitoring.py
@@ -1,14 +1,25 @@
 """ 
-This class is an attempt to resolve a potential global values issue
+This class stores the state of the live game monitoring system.
+
+This state is kept between monitoring loop iterations to track wether a game is currently active.
 """
 
 class MonitoringState:
 
-    def __self__(self):
+    def __init__(self):
         self.in_game = False
         self.last_event_id = -1
+        self.events = []
         
 
     def start_game(self):
-        self.in_game =True
+        self.in_game = True
         
+    
+    def end_game(self):
+        self.in_game = False
+
+    def reset(self):
+        self.in_game = False
+        self.last_event_id = -1
+        self.events.clear()
